270a0ad9bc643e96c75e29eee88d7ef29b66a6d9
thiago <thiagoxd_008@hotmail.com>
2021-10-24 17:17:46 -0300

    vue and corsheaders integration

---
+++ frontend/src/views/Posts.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="posts">
+      <div class="album py-5 bg-light">
+          <div class="container">
+            <div class="row">
+              <div v-for="posts in APIData" :key="posts.id" class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" alt="Card image cap">
+                  <div class="card-body">
+                      <h4 class=""><a class="text-secondary" href="">{{posts.title}}</a></h4>
+                      <p class="card-text">{{posts.content}}</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                      <a href="" class="btn btn-sm btn-outline-primary" role="button" aria-pressed="true">View</a>
+                      <a href="" class="btn btn-sm btn-outline-secondary" role="button" aria-pressed="true">Edit</a>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+  import { getAPI } from '../axios-api'
+  export default {
+    name: 'Posts',
+    data () {
+      return {
+          APIData: []
+        }
+    },
+    created () {
+        getAPI.get('/posts/',)
+          .then(response => {
+            console.log('Post API has recieved data')
+            this.APIData = response.data
+          })
+          .catch(err => {
+            console.log(err)
+          })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
